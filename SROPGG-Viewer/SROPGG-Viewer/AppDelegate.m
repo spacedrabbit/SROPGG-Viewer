9229d0e5d261891996b81981789003dde1e2dc55
Louis Tur <louis.tur@gmail.com>
2015-05-18 23:40:51 -0400

    Basic open menu in place.

--- SROPGG-Viewer/SROPGG-Viewer/AppDelegate.m
+++ SROPGG-Viewer/SROPGG-Viewer/AppDelegate.m
@@ -11,16 +11,48 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSView *fileBrowserWindow;
+@property (weak) IBOutlet NSButton *openFileBtn;
+@property (weak) IBOutlet NSTextField *fileLocationLabel;
+
+- (IBAction)openFileBroser:(NSButton *)sender;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    
+    
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (IBAction)openFileBroser:(NSButton *)sender {
+    
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    openPanel.delegate = self;
+    openPanel.title = @"Select OP.GG file to Open";
+    openPanel.allowedFileTypes = @[ @"cmd" ];
+    openPanel.allowsMultipleSelection = YES;
+    
+    [openPanel beginWithCompletionHandler:^(NSInteger result) {
+        
+        if (result == NSFileHandlingPanelOKButton) {
+            NSArray *selectedDocument = [openPanel URLs];
+            
+        }
+        
+    }];
+
+}
+
+- (void) presentSelectedDirectory:(NSURL *)directory inTextField:(NSTextField *)textField{
+    
+    
 }
 
 @end
